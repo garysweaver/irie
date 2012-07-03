1c02748684028cb61b174e79a5633c9d90a259cb
Gary S. Weaver <garysweaver@gmail.com>
2012-07-03 17:43:32 -0400

    Implementing CORS as optional, configurable. Adding documentation.

--- README.md
+++ README.md
@@ -26,22 +26,96 @@ So if you had an existing model app/models/foobar.rb:
     class Foobar < ActiveRecord::Base
     end
 
-If you have a Rails 3 controller and a Foobar model, you'd just extend our automatically configuring controller and it would automatically provide:
+You would do this in app/controllers/foobar_controller.rb:
 
     class FoobarController < RestfulJson::Controller
     end
 
-Then in config/routes.rb, add:
+Then in config/routes.rb, you would add:
 
     resources :foobar
 
 That's it. Now you can serve up some Javascript in one of your views that hits the RESTful services that have been defined.
 
+### The JSON Services
+
+The first part is just basic stuff from a resourceful routes in Rails 3, but it might help.
+
+You'd start the server:
+
+    rails s
+
+You could then list all Foobars with a GET to:
+
+    http://localhost:3000/foobar.json
+
+To find out what the JSON to use as the 'foobar' parameter value, you could create one first in Rails console and then get that or call to_json on it in the console.
+
+Get a specific foobar using a GET to:
+
+    http://localhost:3000/foobar/object_id.json
+
+POST the JSON of a new Foobar as request parameter 'foobar' to create one:
+
+    http://localhost:3000/foobar/new.json
+
+Update a Foobar with a PUT call to:
+
+    http://localhost:3000/foobar/object_id.json
+
+And destroy it with a DELETE call to:
+
+    http://localhost:3000/foobar/object_id.json
+
+You might also look at the output of 'rake routes' to see the paths for /foobar and then construct URLs to test it:
+
+    rake routes
+
+#### CORS
+
+If you have javascript/etc. code in the client that is running under a different host or port than Rails server, then you are cross-origin/cross-domain and we handle this with [CORS][cors].
+
+By default CORS is disabled, so to enable it you can either set the environment variable RESTFUL_JSON_CORS_GLOBALLY_ENABLED, or in config/environment.rb or for a specific environment like config/environments/development.rb you can add the following global variable:
+
+    $restful_json_cors_globally_enabled = true
+
+By default, we make CORS just allow everything, so the whole cross-origin/cross-domain thing goes away and you can get to developing locally with your Javascript app that isn't even being served by Rails.
+
+##### Customizing CORS
+
+So, if you enabled CORS, then CORS starts with a [preflight request][preflight_request] from the client (the browser), to which we respond with a response. You can customize the values of headers returned in the :cors_preflight_headers option. Then for all other requests to the controller, you can specify headers to be returned in the :cors_access_control_headers option.
+
+Here's an example of customizing both:
+
+    class MyModelController < RestfulJson::Controller
+      def initialize
+        restful_json_options({
+          cors_preflight_headers: {
+            'Access-Control-Allow-Origin':  '*',
+            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, X-Prototype-Version',
+            'Access-Control-Max-Age': '1728000'
+          },
+          cors_access_control_headers: {
+            'Access-Control-Allow-Origin':  '*',
+            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+            'Access-Control-Max-Age': '1728000'
+          }
+        })
+        super
+      end
+    end
+
+### Future
+
+Filter
+
 #### Using a Different Model
 
-I wanted to just do it in the class, but because of RestfulJson::BaseController looking 
+I wanted to just do it in the class vs. initialize so it would look cleaner, but I think this makes things overall a lot clearer considering that that RestfulJson::Controller also looks at its name to determine model in its initializer:
 
-    class TestAnotherModelController < RestfulJson::BaseController
+    # Note that we're using BaseController here. It doesn't try to guess the model class, etc.
+    class MyModelController < RestfulJson::BaseController
       def initialize
         restful_json_model TestModel
         
@@ -57,4 +131,6 @@ Copyright (c) 2012 Gary S. Weaver, released under the [MIT license][lic].
 
 [ember_data_example]: https://github.com/dgeb/ember_data_example/blob/master/app/controllers/contacts_controller.rb
 [angular]: http://angularjs.org/
+[cors]: http://enable-cors.org/
+[preflight_request]: http://www.w3.org/TR/cors/#resource-preflight-requests
 [lic]: http://github.com/garysweaver/activerecord-attribute-override/blob/master/LICENSE
--- lib/restful_json/base_controller.rb
+++ lib/restful_json/base_controller.rb
@@ -1,5 +1,8 @@
 class RestfulJson::BaseController < ApplicationController
 
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
   # note: model_class can be fully-qualified like MyModule::MySubModule::MyClass
   def restful_json_model(model_class)
     # Ensure that controller is not repurposed for different models
@@ -20,9 +23,60 @@ class RestfulJson::BaseController < ApplicationController
     @__restful_json_initialized = true
   end
 
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain. Modified from Tom Sheffler's example in 
+  # http://www.tsheffler.com/blog/?p=428 to allow customization.
+  def cors_preflight_check
+    unless ENV['RESTFUL_JSON_CORS_GLOBALLY_ENABLED'] || $restful_json_cors_globally_enabled
+      if request.method == :options
+        headers['Access-Control-Allow-Origin'] =  '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+        headers['Access-Control-Max-Age'] = '1728000'
+        # Allow one or more headers to be overriden
+        headers.merge!(@__restful_json_options[:cors_preflight_headers])
+        # CORS returns as text to the browser as a step before the json, so is intentionally text type and not json.
+        render :text => '', :content_type => 'text/plain'
+      end
+    end
+  end
+
+  # For all responses in this controller, return the CORS access control headers.
+  # Modified from Tom Sheffler's example in http://www.tsheffler.com/blog/?p=428
+  # to allow customization.
+  def cors_set_access_control_headers
+    unless ENV['RESTFUL_JSON_CORS_GLOBALLY_ENABLED'] || $restful_json_cors_globally_enabled
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Max-Age'] = '1728000'
+      # Allow one or more headers to be overriden
+      headers.merge!(@__restful_json_options[:cors_access_control_headers])
+    end
+  end
+
   def index
     return if restful_json_controller_not_yet_configured?
-    value = @__restful_json_class.all
+
+    # TODO: continue to explore filtering, etc. and look into extension of this project to use Sunspot/SOLR.
+    # TODO: Darrel Miller/Ted M. Young suggest reviewing these: http://stackoverflow.com/a/4028874/178651
+    #       http://www.ietf.org/rfc/rfc3986.txt  http://tools.ietf.org/html/rfc6570
+    # TODO: Paging. Eugen Paraschiv (a.k.a. baeldung) has a good post here, even though is in context of Spring:
+    #       http://www.baeldung.com/2012/01/18/rest-pagination-in-spring/
+    #       http://www.iana.org/assignments/link-relations/link-relations.
+    #       More on Link header: http://blog.steveklabnik.com/posts/2011-08-07-some-people-understand-rest-and-http
+    #       example of Link header:
+    #       Link: </path/to/resource?other_params_go_here&page=2>; rel="next", </path/to/resource?other_params_go_here&page=9999>; rel="last"
+    #       will_paginate looks like it might be a good match
+    
+    # Using scoped and separate wheres if params present similar to solution provided by
+    # John Gibb in http://stackoverflow.com/a/5820947/178651
+    value = @__restful_json_class.scoped
+    allowed_activerecord_model_attribute_keys.each do |attribute_key|
+      param = params[attribute_key]
+      value = value.where(attribute_key => param) if param.present?
+    end
+
     instance_variable_set("@#{@__restful_json_model_plural}".to_sym, value)
     respond_to do |format|
       format.json { render json: {@__restful_json_model_plural.to_sym => value} }
@@ -78,7 +132,7 @@ class RestfulJson::BaseController < ApplicationController
 
 private
 
-  attr_accessor :__restful_json_class, :__restful_json_initialized, :__restful_json_model_plural, :__restful_json_model_singular
+  attr_accessor :__restful_json_class, :__restful_json_initialized, :__restful_json_model_plural, :__restful_json_model_singular, :__restful_json_options
 
   # Stub out actions until dynamically implemented, otherwise will get something like:
   # AbstractController::ActionNotFound (The action 'index' could not be found for SomeModelController):
@@ -93,5 +147,14 @@ private
         format.json { render json: nil, status: :service_unavailable }
       end
     end
+  end
+  
+  def restful_json_options(options)
+    @__restful_json_options = options
+  end
+
+  def allowed_activerecord_model_attribute_keys
+    # Solution from Jeffrey Chupp (a.k.a. 'semanticart') in http://stackoverflow.com/a/1526328/178651
+    @__restful_json_class.new.attributes.keys - @__restful_json_class.protected_attributes.to_a
   end
 end

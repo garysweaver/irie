98672d916741368cb7399f1ee1080ebc9cd460ba
Gary S. Weaver <garysweaver@gmail.com>
2012-08-09 13:32:13 -0400

    version 2.4.2. adding support for NULL, null, nil as filter param value

--- lib/restful_json/controller.rb
+++ lib/restful_json/controller.rb
@@ -31,6 +31,11 @@ module RestfulJson
       def arel_predication_split
         '!'
       end
+
+      # as method so can be overriden
+      def convert_request_param_value_for_filtering(attr_name, value)
+        value && ['NULL','null','nil'].include?(value) ? nil : value
+      end
       
       # as method so can be overriden
       def multiple_value_arel_predications(attr_name=nil)
@@ -252,13 +257,12 @@ module RestfulJson
         # handle foo=bar, foo^eq=bar, foo^gt=bar, foo^gteq=bar, etc.
         allowed_activerecord_model_attribute_keys.each do |attribute_key|
           puts "Finding #{restful_json_model_class}"
-          param = params[attribute_key]
-          value = value.where(attribute_key => param) if param.present?
+          value = value.where(attribute_key => convert_request_param_value_for_filtering(param)) if param.present?
           # supported AREL predications are suffix of ^ and predication in the parameter name
           supported_arel_predications.each do |arel_predication|
             param = params["#{attribute_key}#{arel_predication_split}#{arel_predication}"]
             if param.present?
-              one_or_more_param = multiple_value_arel_predications.include?(arel_predication) ? param.split(value_split) : param
+              one_or_more_param = multiple_value_arel_predications.include?(arel_predication) ? param.split(value_split).collect{|v|convert_request_param_value_for_filtering(v)} : convert_request_param_value_for_filtering(param)
               puts ".where(value[#{attribute_key.to_sym.inspect}].call(#{arel_predication.to_sym.inspect}, '#{one_or_more_param}'))"
               value = value.where(t[attribute_key.to_sym].try(arel_predication.to_sym, one_or_more_param))
             end

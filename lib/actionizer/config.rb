bf76275bfefc977fafe809bae6c90e415c83ac36
Gary S. Weaver <garysweaver@gmail.com>
2013-10-15 13:16:57 -0400

    updating config format

--- lib/actionizer/config.rb
+++ lib/actionizer/config.rb
@@ -29,8 +29,8 @@ Actionizer.configure do
   # Delimiter for values in request parameter values.
   self.filter_split = ','
 
-  # Use one or more alternate request parameter names for functions,
-  # e.g. self.function_param_names = {distinct: :very_distinct, limit: [:limit, :limita]}
+  # Use one or more alternate request parameter names for functions, e.g.
+  # `self.function_param_names = {distinct: :very_distinct, limit: [:limit, :limita]}`
   self.function_param_names = {}
   
   # Delimiter for ARel predicate in the request parameter name.
@@ -42,25 +42,12 @@ Actionizer.configure do
   # Used when paging is enabled.
   self.number_of_records_in_a_page = 15
 
-  # Actions that you can implement in the controller via include_actions,
-  # e.g. include_actions :index, :show
-  # Each is added as each file is required when the gem is loaded, so for a full list,
-  # check ::Actionizer.available_actions in rails c.
-  # You shouldn't have to worry about configuring this typically.
-  self.available_actions = {}
-
-  # Extensions to actions that you can implement in the controller via include_extensions,
-  # e.g. include_extensions :count, :paging
-  # Each is added as each file is required when the gem is loaded, so for a full list,
-  # check ::Actionizer.available_extensions in rails c.
-  # You shouldn't have to worry about configuring this typically.
-  self.available_extensions = {}
-
   # When you include the defined action module, it includes the associated modules.
-  # If value or value array contains symbol it will look up symbol in self.available_extensions in the controller
-  # (which is defaulted to ::Actionizer.available_extensions).
-  # If value is String will assume String is the fully-qualified module name to include, e.g. index: '::My::Module'
-  # If constant, it will just include constant (module), e.g. index: ::My::Module
+  # If value or value array contains symbol it will look up symbol in 
+  # self.available_extensions in the controller (which is defaulted to 
+  # `::Actionizer.available_extensions`). If value is String will assume String is the
+  # fully-qualified module name to include, e.g. `index: '::My::Module'`, If constant,
+  # it will just include constant (module), e.g. `index: ::My::Module`.
   self.autoincludes = {
     create: [:query_includes, :render_options],
     destroy: [:query_includes, :render_options],
@@ -71,7 +58,25 @@ Actionizer.configure do
     update: [:query_includes, :render_options]
   }
 
-  # By default, it sets the instance variable, but does not return entity if request update (e.g. in JSON format).
+  # By default, it sets the instance variable, but does not return entity if request
+  # update, e.g. in JSON format.
   self.update_should_return_entity = false
-end
 
+  # Actions that you can implement in the controller via `include_actions`,
+  # e.g. `include_actions :index, :show`
+  # Each is added as each file is required when the gem is loaded, so for a full list,
+  # check `::Actionizer.available_actions` in rails console.
+  # You shouldn't have to worry about configuring this typically.
+  self.available_actions = {}
+
+  # Extensions to actions that you can implement in the controller via
+  # `include_extensions`, e.g. `include_extensions :count, :paging`
+  # Each is added as each file is required when the gem is loaded, so for a full list,
+  # check `::Actionizer.available_extensions` in rails console.
+  # You shouldn't have to worry about configuring this typically.
+  self.available_extensions = {}
+
+  # If true, will logger.debug in instance methods to help with execution tracing at
+  # runtime.
+  self.debug = false
+end
